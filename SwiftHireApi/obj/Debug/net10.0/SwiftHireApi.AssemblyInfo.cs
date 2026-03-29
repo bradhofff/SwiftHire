2c71dc8e7244cf16c00e7a88103a8d9a14d058d6
bradhofff <bradhofff@gmail.com>
2026-03-29 15:19:22 -0400

    Removing mock jobs to integrate api

--- SwiftHireApi/obj/Debug/net10.0/SwiftHireApi.AssemblyInfo.cs
+++ SwiftHireApi/obj/Debug/net10.0/SwiftHireApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwiftHireApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a353bc8f0051a500716a4436addbbf05958bcef")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwiftHireApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwiftHireApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
